23caee983f06f2ef669f806104c13395916c8b54
Seum <26SeumP@mydacc.org>
2026-01-08 14:13:54 -0500

    Finished HAT page, fixed images not existing, etc. - PS - 2:13PM 1/8/2026

--- js/access.js
+++ js/access.js
@@ -27,7 +27,6 @@ toggleText.addEventListener("click", () => {
 
 toggleFont.addEventListener("click", () => {
   document.body.classList.toggle("dyslexia");
-  document.getElementsByClassName("midnav").style.fontSize
 });
 
 toggleMouse.addEventListener("click", () => {
